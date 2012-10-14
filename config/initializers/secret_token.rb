a9458cc4b114eac437c8ddd4e79e8cccbd15e481
k1w1 <k1w1@k1w1.org>
2012-10-13 20:15:37 -0700

    * Created empty Rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InlineEditDemo::Application.config.secret_token = '8e3d9fedffa188a1dfe8c00b150d1fe7aa166206eeb7cd6e0d98f364c6d75acaaa5af1d84a3e3125e1d8fe1b0d554a57484ec4e56c09aeb4f5c78c0cb6f41903'
